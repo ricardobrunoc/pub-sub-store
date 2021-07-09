47b90b153fb5ca60f68ea5f5e685aed26c393286
Ricardo Bruno <ricardobrunoc@yahoo.com.br>
2021-07-09 20:49:29 -0300

    Processo de report

--- services/report/app.js
+++ services/report/app.js
@@ -23,8 +23,20 @@ async function printReport() {
       }
 }
 
+async function processMessage(msg) {
+    const orderData = JSON.parse(msg.content)
+    try {
+        updateReport(orderData.products)
+        printReport();
+        console.log(`RELATORIO GERADO`)
+    } catch (error) {
+        console.log(`X ERROR TO PROCESS: ${error.response}`)
+    }
+}
+
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
+    console.log(`INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`)
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {processMessage(msg)})
 } 
 
 consume()
